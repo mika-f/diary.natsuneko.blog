48cbf3841ac3629a242361a58d641265a5e95b75
Natsune Mochizuki <me@natsuneko.cat>
2023-06-24 19:01:16 +0900

    chore: initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import type { NextraBlogTheme } from "nextra-theme-blog";
+
+export default {
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  navs: [
+    {
+      url: "https://twitter.com/6jz",
+      name: "@6jz",
+    },
+  ],
+  darkMode: true,
+  footer: <p>© 2023 Natsuneko.</p>,
+  readMore: "Read More →",
+} satisfies NextraBlogTheme;
